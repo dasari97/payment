affb29985ecc4f62021ccdee959b7842004b45b4
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-30 08:26:39 +0000

    adding new file

--- Terraform-Mutable/main.tf
+++ Terraform-Mutable/main.tf
@@ -5,4 +5,7 @@ module "payment" {
     od_ins = var.od_ins
     instance_type = var.instance_type
     component   = "payment"
+    port        = 8080
+    is_internal = "true"
+    priority    = 600
 }
